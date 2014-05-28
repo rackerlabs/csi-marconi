41dfdc8156ea78f4a990537af48992d7a0aa7314
Obulapathi N Challa <obulpathi@gmail.com>
2014-05-28 14:18:57 +0000

    cleaning up queue names

--- create_queues.py
+++ create_queues.py
@@ -1,14 +1,17 @@
+from __future__ import print_function
+
 import os
 import pyrax
 import pyrax.exceptions as exc
 
-pyrax.set_setting("identity_type", "rackspace")
+pyrax.set_environment('rackspace_cloud')
+pyrax.set_default_region('hkg')
 creds_file = os.path.expanduser("~/.credentials.conf")
 pyrax.set_credential_file(creds_file)
 
-with open("existingqueue.csv") as names:
+with open(os.path.expanduser("~/.tsung/existingqueue.csv")) as names:
     for name in names:
         try:
-            queue = pyrax.queues.create(name)
+            queue = pyrax.queues.create(name.rstrip())
         except exc.DuplicateQueue:
             pass
